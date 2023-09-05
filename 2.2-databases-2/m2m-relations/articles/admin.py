48befb28017e362c6a49995beeb2e89b4acf8cbd
Nik Bezmaternykh <nikitabit@yandex.ru>
2023-09-05 18:29:47 +0700

    init

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            cleaned = form.cleaned_data
+            if cleaned['is_main']:
+                counter += 1
+        if counter != 1:
+            raise ValidationError('Должен быть один основной тэг')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 0
+    formset = ScopeInlineFormset
+
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    ordering = ['title']

c6f23d40e75ec801b9aa6cdfa6da18e88f218a07
Nik Bezmaternykh <nikitabit@yandex.ru>
2023-09-23 17:55:22 +0700

    init

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,5 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
+from logistic.filters import ProductFilter
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
@@ -7,10 +10,15 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_fields = ['title', 'description']
     # при необходимости добавьте параметры фильтрации
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    # filterset_fields = ['products']
+    search_fields = ['products']
+    filterset_class = ProductFilter
     # при необходимости добавьте параметры фильтрации

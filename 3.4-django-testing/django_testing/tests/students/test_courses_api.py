d349110e6eb12892f0114eede2babfad4fd9a6f6
Nik Bezmaternykh <nikitabit@yandex.ru>
2023-10-04 11:45:40 +0700

    Change ids to values from factory Add too_much test

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.django_db
 def test_get_course(client, courses_factory):
     courses = courses_factory(_quantity=1)
-    response = client.get('/api/v1/courses/1/')
+    response = client.get(f'/api/v1/courses/{courses[0].id}/')
     assert response.status_code == 200
 
     data = response.json()
@@ -24,10 +24,10 @@ def test_get_courses(client, courses_factory):
 @pytest.mark.django_db
 def test_filter_courses_by_id(client, courses_factory):
     courses = courses_factory(_quantity=1000)
-    response = client.get('/api/v1/courses/?id=100')
+    response = client.get(f'/api/v1/courses/?id={courses[100].id}')
     assert response.status_code == 200
     data = response.json()
-    assert data[0]['id'] == 100
+    assert data[0]['id'] == courses[100].id
 
 
 @pytest.mark.django_db
@@ -59,15 +59,15 @@ def test_create_course(client, student_factory):
 
 @pytest.mark.django_db
 def test_update_course(client, student_factory, courses_factory):
-    courses_factory(_quantity=10, id=1)
+    courses = courses_factory(_quantity=10, id=1)
     students = student_factory(_quantity=10)
 
     data = {
         'name': 'UPDATED NAME',
         'students': [student.id for student in students]
     }
-
-    response = client.patch('/api/v1/courses/1/', data=data, format='json')
+    course_id = courses[0].id
+    response = client.patch(f'/api/v1/courses/{course_id}/', data=data, format='json')
 
     assert response.status_code == 200
     response = response.json()
@@ -78,20 +78,21 @@ def test_update_course(client, student_factory, courses_factory):
 
 @pytest.mark.django_db
 def test_delete_course(client, courses_factory):
-    courses_factory(_quantity=10, id=1)
-    response = client.delete('/api/v1/courses/1/')
+    courses = courses_factory(_quantity=10)
+
+    response = client.delete(f'/api/v1/courses/{courses[0].id}/')
     assert response.status_code == 204
 
 
 @pytest.mark.parametrize(
     ['max'], (
-            ('1',),
-            ('10',),
+            ('20',),
+            ('50',),
             ('100',)
     )
 )
 @pytest.mark.django_db
-def test_max_students_course(client, student_factory, max, settings):
+def test_max_students_in_course(client, student_factory, max, settings):
     students = student_factory(_quantity=20)
     settings.MAX_STUDENTS_PER_COURSE = int(max)
     data = {
@@ -105,3 +106,21 @@ def test_max_students_course(client, student_factory, max, settings):
 
     assert response['name'] == "TEST"
     assert len(response['students']) <= 20
+
+
+@pytest.mark.parametrize(
+    ['max'], (
+            ('19',),
+            ('1',)
+    )
+)
+@pytest.mark.django_db
+def test_too_much_students_course(client, student_factory, max, settings):
+    students = student_factory(_quantity=20)
+    settings.MAX_STUDENTS_PER_COURSE = int(max)
+    data = {
+        'name': 'TEST',
+        'students': [student.id for student in students]
+    }
+    response = client.post('/api/v1/courses/', data=data, format='json')
+    assert not response.status_code == 201
